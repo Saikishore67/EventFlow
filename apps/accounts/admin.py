2ec887bf9f819fedb6a3c7b8a266e5a51ac48418
Sai Kishore Saravanan <saravanansaikishore67@gmail.com>
2026-01-11 19:36:17 +0530

    Showing is_organizer and is_attendee - information in Admin UI dashboard

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -4,7 +4,7 @@ from .models import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'is_staff', 'is_active')
+    list_display = ('id', 'username', 'email', 'is_staff', 'is_active', 'is_organizer', 'is_attendee')
     search_fields = ('username', 'email')
     list_filter = ('is_staff', 'is_active')
     ordering = ('id',)
